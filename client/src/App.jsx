612dab9f9127c68733aca9ebfd360a5800c469ba
sathegoodkid <sa@gmail.com>
2025-06-07 13:38:17 +0700

    complete basic backend

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+// import Function from "./components/Function";
+import Login from "./components/Login";
+export default function App() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+    </>
+  );
+}
