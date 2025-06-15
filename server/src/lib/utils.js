01886de9b394e30d9f347ed4db421d4ba4d66cbb
sakaka1234 <danghoangsa2006@gmail.com>
2025-06-15 22:51:43 +0700

    last timeeee 3

--- server/src/lib/utils.js
+++ server/src/lib/utils.js
@@ -7,7 +7,8 @@ const generateToken = (userId, res) => {
   //send token
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true, //prevent XSS attacks cross-site scripting attacks
+    sameSite: "None",
+    secure: true,
   });
 
   return token;
